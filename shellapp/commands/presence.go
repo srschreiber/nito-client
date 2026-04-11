8dc0ee799e5ea060326c4e1af784b7f3452afb86
Schreiber <sam.schreiber@sublimesecurity.com>
2026-04-11 14:11:56 -0700

    better notifications

---
+++ shellapp/commands/presence.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2026 Sam Schreiber
+// SPDX-License-Identifier: MIT
+
+package commands
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	wstypes "github.com/srschreiber/nito-client/shared/websocket_types"
+	"github.com/srschreiber/nito-client/shellapp/clientlog"
+	"github.com/srschreiber/nito-client/shellapp/connection"
+	"github.com/srschreiber/nito-client/shellapp/keys"
+)
+
+func sendPresence(rpcName string, payload any) {
+	s := connection.CurrentSession()
+	if s == nil {
+		return
+	}
+	p, err := json.Marshal(payload)
+	if err != nil {
+		clientlog.Error("presence: marshal %s: %v", rpcName, err)
+		return
+	}
+	sig, err := keys.Sign(s.UserID+":"+rpcName, s.UserID)
+	if err != nil {
+		clientlog.Error("presence: sign %s: %v", rpcName, err)
+		return
+	}
+	msg := wstypes.ToBrokerWsMessage{
+		RPCName:   rpcName,
+		RequestID: fmt.Sprintf("%d", time.Now().UnixNano()),
+		UserID:    s.UserID,
+		Nonce:     fmt.Sprintf("%d", time.Now().UnixNano()),
+		Timestamp: time.Now().Unix(),
+		Signature: sig,
+		Payload:   p,
+	}
+	data, _ := json.Marshal(msg)
+	if err := connection.Send(data); err != nil {
+		clientlog.Error("presence: send %s: %v", rpcName, err)
+	}
+}
+
+// SendRoomEnter notifies the broker the user is now viewing the given room.
+func SendRoomEnter(roomID string) {
+	sendPresence(wstypes.RPCRoomEnter, wstypes.RoomEnterPayload{RoomID: roomID})
+}
+
+// SendRoomLeave notifies the broker the user navigated away from the given room.
+func SendRoomLeave(roomID string) {
+	sendPresence(wstypes.RPCRoomLeave, wstypes.RoomLeavePayload{RoomID: roomID})
+}
